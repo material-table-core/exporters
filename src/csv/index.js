320d16aa7508a7b05f0db7f497a0697d7eabc061
Matt Oestreich <matthewpoestreich@gmail.com>
2021-02-20 19:36:01 -0600

    Update index.js

--- src/csv/index.js
+++ src/csv/index.js
@@ -2,16 +2,25 @@ import { CsvBuilder } from 'filefy';
 
 export default function ExportCsv(
   columns,
-  data,
+  data = [],
   filename = 'data',
   delimiter = ','
 ) {
   try {
+    let finalData = data;
+    // Grab first item for data array, make sure it is also an array.
+    // If it is an object, 'flatten' it into an array of strings.
+    if (data.length && !Array.isArray(data[0])) {
+      if (typeof data[0] === 'object') {
+      // Turn data into an array of string arrays, without the `tableData` prop
+      finalData = data.map(({ tableData, ...row }) => Object.values(row));  
+      }
+    }
     const builder = new CsvBuilder(filename + '.csv');
     builder
       .setDelimeter(delimiter)
       .setColumns(columns.map((col) => col.title))
-      .addRows(Array.from(data))
+      .addRows(Array.from(finalData))
       .exportFile();
   } catch (err) {
     console.error(`error in ExportCsv : ${err}`);
