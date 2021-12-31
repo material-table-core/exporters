a531aef2e7fb8c4541c85c9156d630ae73b09d65
Domino987 <domino987@gmail.com>
2021-12-31 02:20:45 +0100

    improve types

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,2 +1,17 @@
-export function ExportCsv(columns: Array<any>, data: Array<any>, filename: string, delimeter?: string): void;
-export function ExportPdf(columns: Array<any>, data: Array<any>, filename: string): void;
+type IColum<T> = {
+  title: string;
+  field: string;
+  exportTransformer?: (row: T) => unknown;
+};
+
+export function ExportCsv<T>(
+  columns: Array<IColum<T>>,
+  data: Array<T>,
+  filename: string,
+  delimeter?: string
+): void;
+export function ExportPdf(
+  columns: Array<IColum<T>>,
+  data: Array<T>,
+  filename: string
+): void;
