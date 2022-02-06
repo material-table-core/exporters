c343cc8e4effe317742d04f1a6ff1254ab1798e2
Domino987 <domino987@gmail.com>
2022-02-06 16:58:33 +0100

    bug: correct spelling of delimiter

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,17 +1,13 @@
 import { Column } from "@material-table/core";
 
-interface IColum<T extends object> extends Column<T> {
-  exportTransformer?: (row: T) => unknown;
-}
-
 export function ExportCsv<T extends object>(
-  columns: Array<IColum<T>>,
+  columns: Array<Column<T>>,
   data: Array<T>,
   filename: string,
-  delimeter?: string
+  delimiter?: string
 ): void;
 export function ExportPdf<T extends object>(
-  columns: Array<IColum<T>>,
+  columns: Array<Column<T>>,
   data: Array<T>,
   filename: string
 ): void;
